534853711bcd3153de9d6a605a1c37a0913e0b13
Sunny <sjbeatteay@gmail.com>
2016-12-07 12:59:40 -0500

    added helper method

--- mint.rb
+++ mint.rb
@@ -17,12 +17,27 @@ before do
   session[:expenses] ||= []
   session[:assets] ||= []
   session[:liabilities] ||= []
+  session[:single] = nil
+  session[:joint] = nil
+  session[:separate] = nil
+  session[:head] = nil
+  session[:widow] = nil
 end
 
 helpers do
   def next_id(elements)
     max = elements.map { |element| element[:id] }.max || 0
     max + 1
+  end
+
+  def add_commas(amount)
+    result = []
+    reversed_amount = amount.to_s.chars.reverse
+    until reversed_amount.empty?
+      result << reversed_amount.shift(3)
+      result << [','] unless reversed_amount.empty?
+    end
+    result.flatten.reverse.join
   end
 
   def load_list_info(list)
@@ -37,6 +52,71 @@ helpers do
 
     [list_type, list_name, description]
   end
+
+  def determine_tax_bracket(status, incomes, expenses)
+    net_income = incomes - expenses
+
+    case status
+    when '' || 'pick' then nil
+    when 'single' then calculate_single(net_income)
+    when 'joint' then calculate_joint_widow(net_income)
+    when 'separate' then calculate_separate(net_income)
+    when 'head' then calculate_head(net_income)
+    when 'widow' then calculate_joint_widow(net_income)
+    end
+  end
+
+  def calculate_single(net)
+    return "0%" if net < 0
+    return "35% - 39.6%" if net >= 415051
+    case net
+    when (0...9276)        then "0% - 10%"
+    when (9276...37651)    then "10% - 15%"
+    when (37651...91151)   then "15% - 25%"
+    when (91151...190151)  then "25% - 28%"
+    when (190151...413351) then "28% - 33%"
+    when (413351...415051) then "33-35%"
+    end
+  end
+
+  def calculate_joint_widow(net)
+    return "0%" if net < 0
+    return "35% - 39.6%" if net >= 466951
+    case net
+    when (0...18551)        then "0% - 10%"
+    when (18851...75301)    then "10% - 15%"
+    when (75301...151901)   then "15% - 25%"
+    when (151901...231451)  then "25% - 28%"
+    when (231451...413351) then "28% - 33%"
+    when (413351...466951) then "33-35%"
+    end
+  end
+
+  def calculate_separate(net)
+    return "0%" if net < 0
+    return "35% - 39.6%" if net >= 233476
+    case net
+    when (0...9276)        then "0% - 10%"
+    when (9276...37651)    then "10% - 15%"
+    when (37651...75951)   then "15% - 25%"
+    when (75951...115726)  then "25% - 28%"
+    when (115726...206676) then "28% - 33%"
+    when (206676...233476) then "33-35%"
+    end
+  end
+
+  def calculate_head(net)
+    return "0%" if net < 0
+    return "35% - 39.6%" if net >= 441001
+    case net
+    when (0...13251)        then "0% - 10%"
+    when (13251...50401)    then "10% - 15%"
+    when (50401...130151)   then "15% - 25%"
+    when (130151...210801)  then "25% - 28%"
+    when (210801...413351) then "28% - 33%"
+    when (413351...441001) then "33-35%"
+    end
+  end
 end
 
 def calculate(category)
@@ -49,6 +129,10 @@ get '/' do
   @expenses = calculate(session[:expenses])
   @assets = calculate(session[:assets])
   @liabilities = calculate(session[:liabilities])
+  @tax_bracket = determine_tax_bracket(params[:status], @incomes, @expenses)
+  if params[:status]
+    session[params[:status].to_sym] = 'selected'
+  end
   erb :index
 end
 
